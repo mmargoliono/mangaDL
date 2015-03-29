5a8f1b33772d853b7a47fa5910dfa4008c43677d
mmargoliono <lie.r.min.g@gmail.com>
2015-03-30 07:20:06 +1100

    Fixed test case

--- test_downloader.py
+++ test_downloader.py
@@ -11,14 +11,14 @@ class TestDownloader(unittest.TestCase):
         self.assertEqual(5, self.downloader.get_omake_number("Vol.9 Ch.87.5"))
         self.assertEqual(5, self.downloader.get_omake_number("Vol.4 Ch.20.5: Omake"))
         self.assertEqual(6, self.downloader.get_omake_number("Vol.10 Ch.96.6: Special Omake - Anime Anguish Monologue"))
-        
+
     def test_page_count(self):
         self.assertEqual(31, self.downloader.get_page_count(self.soup))
 
     def test_image_url(self):
-        self.assertEqual("http://arc.batoto.net/comics/i/iris-zero/02/ala-atra/English/" + \
+        self.assertEqual("http://arc.bato.to/comics/i/iris-zero/02/ala-atra/English/" + \
                          "read4d79b43745b62/%5BAAS_SS%5D%20Iris%20Zero" + \
-                         "%20v01%20ch02_Iris-credits4d79b43772e5a.jpg", 
+                         "%20v01%20ch02_Iris-credits4d79b43772e5a.jpg",
                          self.downloader.get_image_url(self.soup))
 
     def test_chapter_title(self):
@@ -26,11 +26,11 @@ class TestDownloader(unittest.TestCase):
                          self.downloader.get_chapter_title(self.soup))
 
     def test_next_page_url(self):
-        self.assertEqual("http://www.batoto.net/read/_/1496/iris-zero_v1_ch2_by_ala-atra-scans/2",
+        self.assertEqual("http://bato.to/read/_/1496/iris-zero_v1_ch2_by_ala-atra-scans/2",
                          self.downloader.get_next_page_url(self.soup))
 
     def test_next_chapter_url(self):
-        self.assertEqual("http://www.batoto.net/read/_/703/iris-zero_v1_ch3_by_ala-atra-scans",
+        self.assertEqual("http://bato.to/read/_/703/iris-zero_v1_ch3_by_ala-atra-scans",
                          self.downloader.get_next_chapter_url(self.soup))
 
 
